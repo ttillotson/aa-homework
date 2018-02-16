fa94c139557e807df2cd23bffc50a801280fc028
TravisT <travis.don.tillotson@gmail.com>
2018-02-15 20:38:18 -0800

    Create Map::

---
+++ W1D5/map.rb
@@ -0,0 +1,46 @@
+require 'byebug'
+
+class Map
+  attr_reader :a_map
+
+  def initialize
+    @a_map = []
+  end
+
+  def assign(key, value)
+    if a_map.includes?(key)
+      a_map.map do |pair|
+        if pair[0] == key
+          pair[1] = value
+        end
+      end
+    else
+      a_map << [key, value]
+    end
+    a_map
+  end
+
+  def lookup(key)
+    # debugger
+    match = a_map.select{|pair| pair[0] == key}
+    return nil if match.empty?
+    match[0]
+  end
+
+  def remove(key)
+    a_map.each do |pair|
+      a_map.delete(pair) if pair[0] == key
+    end
+  end
+
+  def show
+    a_map
+  end
+
+end
+
+class Array
+  def includes?(key)
+    self.any?{|pair| pair[0] == key}
+  end
+end
