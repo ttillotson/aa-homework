e032d86c5f0409f69bf96ae93b53808bd8ac069f
TravisT <travis.don.tillotson@gmail.com>
2018-02-18 23:01:34 -0800

    Complete methods

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,38 +4,56 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    @sequence_length += 1
+    require_sequence
+    round_success_message unless game_over
 
   end
 
   def show_sequence
-
+    add_random_color
+    seq
   end
 
   def require_sequence
-
+    puts "What was the sequence of the colors"
+    seq.length.times do |i|
+      input = gets.chomp
+      game_over = true unless input == seq[i]
+    end
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "You got it!"
   end
 
   def game_over_message
-
+    puts "That's not the color!"
+    puts "The sequence was #{seq.join(", ")}"
   end
 
   def reset_game
-
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
   end
 end
