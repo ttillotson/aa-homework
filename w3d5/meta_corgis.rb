0efa48d0261a0e67321ddd7cfea6a20087de09fb
TravisT <travis.don.tillotson@gmail.com>
2018-03-02 00:04:50 -0800

    Meta-snacking

--- w3d5/meta_corgis.rb
+++ w3d5/meta_corgis.rb
@@ -110,11 +110,14 @@ class MetaCorgiSnacks
   end
 
   def method_missing(name, *args)
-    # Your code goes here...
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    result = "#{name.capitalize}: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
   end
 
 
   def self.define_snack(name)
-    # Your code goes here...
+
   end
 end
