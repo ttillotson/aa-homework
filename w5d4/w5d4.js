a0d5db35e3878b63bed985b4494132c05f023e73
TravisT <travis.don.tillotson@gmail.com>
2018-03-15 08:52:13 -0700

    Javascript Problems

---
+++ w5d4/w5d4.js
@@ -0,0 +1,7 @@
+function madLib(verb, adj, noun) {
+  console.log("We shall  ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}");
+}
+
+function isSubstring(string, sub) {
+  return string.includes(sub);
+}
