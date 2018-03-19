09bd8bcf512b2ed8fbe857202965a0d32ca3fc89
TravisT <travis.don.tillotson@gmail.com>
2018-03-18 20:46:20 -0700

    Canvas

---
+++ w6d1/canvas_practice/practice.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  canvas.width = 500;
+  canvas.height = 500;
+
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = "purple";
+  ctx.fillRect = (0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(120, 120, 30, 0, 2*Math.PI, true);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = "purple";
+  ctx.fill();
+});
